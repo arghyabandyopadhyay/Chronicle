8ae06cdaf746cf321ac4f1259797b4b318c80414
arghyabandyopadhyay <arghyab13bga@gmail.com>
2021-05-20 02:28:33 +0530

    App bar new modification

--- lib/Widgets/courseList.dart
+++ lib/Widgets/courseList.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
-import 'courseTile.dart';
+import 'courseCardWidget.dart';
 
 class CourseList extends StatefulWidget {
   final List<CourseModel> listItems;
